0a9f39db29ebcd576da59c18279f7f714d28fc3c
Harsh Gupta <56353320+guptaharsh686@users.noreply.github.com>
2024-05-29 00:59:42 +0530

    Create DownwardTrianglePattern.cs

---
+++ Star Patterns/DownwardTrianglePattern.cs
@@ -0,0 +1,58 @@
+
+using System.Diagnostics.Metrics;
+using System.Text;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the rows of triangle pattern : ");
+        int n = int.Parse(Console.ReadLine());
+        int rows = n;
+        //APPROACH 1
+        for (int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < rows; j++)
+            {
+                if(j < rows - i)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        //APPROACH 2
+        //int rows = n;
+        //for (int i = 0; i < rows; i++)
+        //{
+        //    for (int j = 0; j < rows - i; j++)
+        //    {
+        //        Console.Write("*");
+        //    }
+
+        //    for (int k = 0; k < i; k++)
+        //    {
+        //        Console.Write(" ");
+        //    }
+        //    Console.WriteLine();
+        //}
+    }
+}
+
+//          Enter the rows of triangle pattern : 
+//          10
+//          **********
+//          *********
+//          ********
+//          *******
+//          ******
+//          *****
+//          ****
+//          ***
+//          **
+//          *
